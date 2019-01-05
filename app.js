4f54c9736bc813fe8e2609e751979d63aee5dc00
varlet vincent <vrlt.vincent@gmail.com>
2019-01-05 16:34:46 +0100

    body parsed to json - process.env fixed

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const   nodemailer  = require('nodemailer')
         express     = require('express')
+        http        = require('http')
         app         = express()
         bodyParser  = require('body-parser');
 require('dotenv').config();
 
-app.use(bodyParser.urlencoded({extended:true}));
+//app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 //POST ROUTE FROM PORTFOLIO CONTACT
 app.post('/contact', function(req,res){
@@ -15,23 +17,24 @@ app.post('/contact', function(req,res){
         host:'smtp.gmail.com',
         port: 465,
         secure: true,
+        tls: { rejectUnauthorized: false },
         auth: {
-            user: GMAIL_USER,
-            pass: GMAIL_PASS
+            user: process.env.GMAIL_USER,
+            pass: process.env.GMAIL_PASS
         }
     });
 
     mailOpts = {
         from: req.body.name + ' &lt;' + req.body.email + ' &gt;',
-        to: GMAIL_USER,
+        to: process.env.GMAIL_USER,
         subject: 'Nouveau message du formulaire de contact du portfolio',
-        text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
+        text: `${req.body.name} (${req.body.email}) a écrit: ${req.body.message}`
     };
 
     smtpTrans.sendMail(mailOpts, function(error, response) {
         if(error){
             //res.render('contact-failure');
-            res.send('error');
+            res.send(error);
         }else{
             //res.render('contact-success');
             res.send('mail send');
